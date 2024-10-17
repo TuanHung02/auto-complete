f2b703826fb0433aad782f54bb1f865e1548bbdc
TuanHung02 <buituanhung04122002@gmail.com>
2024-10-17 16:59:50 +0700

    fix csc5

--- src/components/MultipleSelect.vue
+++ src/components/MultipleSelect.vue
@@ -1,7 +1,15 @@
 <template>
     <div class="multiple-select" ref="dropdownRef">
         <div class="input-container">
-            <i style="color: rgba(98, 125, 152, 1);" class="fa-solid fa-magnifying-glass"></i>
+            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" width="18"
+                height="18" x="0" y="0" viewBox="0 0 612.01 612.01" style="enable-background:new 0 0 512 512"
+                xml:space="preserve" class="">
+                <g>
+                    <path
+                        d="M606.209 578.714 448.198 423.228C489.576 378.272 515 318.817 515 253.393 514.98 113.439 399.704 0 257.493 0S.006 113.439.006 253.393s115.276 253.393 257.487 253.393c61.445 0 117.801-21.253 162.068-56.586l158.624 156.099c7.729 7.614 20.277 7.614 28.006 0a19.291 19.291 0 0 0 .018-27.585zM257.493 467.8c-120.326 0-217.869-95.993-217.869-214.407S137.167 38.986 257.493 38.986c120.327 0 217.869 95.993 217.869 214.407S377.82 467.8 257.493 467.8z"
+                        fill="#999999" opacity="1" data-original="#000000" class=""></path>
+                </g>
+            </svg>
             <div class="selected-items">
                 <div v-for="item in selectedItems" :key="item.id" class="selected-item">
                     {{ item.name }} <!-- Hiển thị thuộc tính name -->
@@ -159,7 +167,7 @@ export default defineComponent({
     min-height: 32px;
 
     &:hover {
-        border: 1px solid #9cbdce;
+        border: 1px solid rgba(25, 145, 210, 1);
     }
 
     input {
@@ -282,5 +290,20 @@ li.selected:hover {
 /* Handle on hover */
 ::-webkit-scrollbar-thumb:hover {
     background: #526070;
+}
+
+:-ms-input-placeholder {
+    color: rgba(191, 191, 191, 1);
+    /* Internet Explorer 10-11 */
+}
+
+::-ms-input-placeholder {
+    color: rgba(191, 191, 191, 1);
+    /* Microsoft Edge */
+}
+
+::placeholder {
+    color: rgba(191, 191, 191, 1);
+    /* Firefox, Chrome, Opera */
 }
 </style>
